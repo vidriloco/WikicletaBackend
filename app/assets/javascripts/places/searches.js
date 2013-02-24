f30fd55a8226b205a09484362688515dc8865dca
Alejandro Cruz Paz <vidriloco@gmail.com>
2013-02-23 20:10:13 -0600

    Cleaning code not needed

--- app/assets/javascripts/places/searches.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function() {
-	$('#search_place_map').change(function() {
-		$('.map-search-msgs').toggle();
-	});
-});
--- app/assets/javascripts/plain.js
+++
@@ -1,4 +0,0 @@
-
-$(document).ready(function() {
-	$('#slides').superslides({  slide_speed: 'normal', slide_easing: 'linear', hashchange: true, pagination: false });
-});
