7cae782ba592bb97bd1494dfeb156287e41ab970
Alejandro Cruz Paz <vidriloco@gmail.com>
2013-05-16 17:26:31 -0500

    Regenerated assets

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -9,34 +9,34 @@ $(document).ready(function() {
 	LandingRoutes = function() {
 		this.home = function() {
 			$('.nav li').removeClass('active');
-			$.scrollFromMapToDom('.logo-container', 0);
+			$('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
 		}
 		
 		this.whatIs = function() {
 			$('.nav li').removeClass('active');
 			$('.nav li.what-is').addClass('active');
-			$.scrollFromMapToDom('#what-is', 70);
+			$('html,body').animate({scrollTop: $('#what-is').offset().top-70}, 'slow');
 			mixpanel.track("Landing :: On What Is Section");
 		}
 		
 		this.register = function() {
 			$('.nav li').removeClass('active');
 			$('.nav li.register').addClass('active');
-			$.scrollFromMapToDom('#register', 70);
+			$('html,body').animate({scrollTop: $('#register').offset().top-70}, 'slow');
 			mixpanel.track("Landing :: On Registration Section");
 		}
 		
 		this.allies = function() {
 			$('.nav li').removeClass('active');
 			$('.nav li.allies').addClass('active');
-			$.scrollFromMapToDom('#allies', 70);
+			$('html,body').animate({scrollTop: $('#allies').offset().top-70}, 'slow');
 			mixpanel.track("Landing :: On Allies Section");
 		}
 		
 		this.media_ = function() {
 			$('.nav li').removeClass('active');
 			$('.nav li.media_').addClass('active');
-			$.scrollFromMapToDom('#media', 70);
+			$('html,body').animate({scrollTop: $('#media').offset().top-70}, 'slow');
 			mixpanel.track("Landing :: On Media Section");
 		}
 	}
