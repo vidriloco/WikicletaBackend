7e3ebcf0491880122c1ba21ff3d32c032c675bad
Alejandro Cruz Paz <vidriloco@gmail.com>
2013-05-20 13:53:03 -0500

    Added assets to precompile queue and changed fog host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       :rackspace_api_key  => 'ecd0d0ca89092f8299f4d8178ab823bb'
     }
     config.fog_directory = 'wikicleta'
-    config.fog_host = "http://c13774363.r63.cf2.rackcdn.com"
+    config.fog_host = "http://7f81635322f22f9c8430-2c43e3cccc7a9a510413dd8cb2a62365.r57.cf1.rackcdn.com"
   end
 else
   CarrierWave.configure do |config|
