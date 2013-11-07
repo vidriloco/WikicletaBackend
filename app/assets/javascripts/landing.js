845f9a39cd7782d9fe1720e5e1efbd2162afc33a
Alejandro Cruz Paz <vidriloco@gmail.com>
2013-11-07 11:26:00 -0600

    Fully responsive landing page

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -2,52 +2,49 @@
 
 $(document).ready(function() {
 	
-	$('#sign-up').bind('click', function() {
-		mixpanel.track("Landing :: Registration Button");
-	});
-	
-	LandingRoutes = function() {
-		this.home = function() {
-			$('.nav li').removeClass('active');
-			$('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
-		}
-		
-		this.whatIs = function() {
-			$('.nav li').removeClass('active');
-			$('.nav li.what-is').addClass('active');
-			$('html,body').animate({scrollTop: $('#what-is').offset().top-70}, 'slow');
-			mixpanel.track("Landing :: On What Is Section");
-		}
-		
-		this.register = function() {
-			$('.nav li').removeClass('active');
-			$('.nav li.register').addClass('active');
-			$('html,body').animate({scrollTop: $('#register').offset().top-70}, 'slow');
-			mixpanel.track("Landing :: On Registration Section");
-		}
-		
-		this.allies = function() {
-			$('.nav li').removeClass('active');
-			$('.nav li.allies').addClass('active');
-			$('html,body').animate({scrollTop: $('#allies').offset().top-70}, 'slow');
-			mixpanel.track("Landing :: On Allies Section");
-		}
-		
-		this.media_ = function() {
-			$('.nav li').removeClass('active');
-			$('.nav li.media_').addClass('active');
-			$('html,body').animate({scrollTop: $('#media').offset().top-70}, 'slow');
-			mixpanel.track("Landing :: On Media Section");
-		}
-	}
+	var mobileOS;    // will either be iOS, Android or unknown
+	var mobileOSver; // this is a string, use Number(mobileOSver) to convert
 
-	var landingRoutes = new LandingRoutes();
-	Path.map("#/").to(landingRoutes.home);
-	Path.map("#/what-is").to(landingRoutes.whatIs);
-	Path.map('#/register').to(landingRoutes.register);
-	Path.map('#/allies').to(landingRoutes.allies);
-	Path.map('#/media').to(landingRoutes.media_);
+	function getOS()
+	{
+	  var ua = navigator.userAgent;
+	  var uaindex;
+
+	  // determine OS
+	  if ( ua.match(/iPad/i) || ua.match(/iPhone/i) )
+	  {
+	    mobileOS = 'iOS';
+	    uaindex  = ua.indexOf( 'OS ' );
+	  }
+	  else if ( ua.match(/Android/i) )
+	  {
+	    mobileOS = 'Android';
+	    uaindex  = ua.indexOf( 'Android ' );
+	  }
+	  else
+	  {
+	    mobileOS = 'unknown';
+	  }
+
+	  // determine version
+	  if ( mobileOS === 'iOS'  &&  uaindex > -1 )
+	  {
+	    mobileOSver = ua.substr( uaindex + 3, 3 ).replace( '_', '.' );
+	  }
+	  else if ( mobileOS === 'Android'  &&  uaindex > -1 )
+	  {
+	    mobileOSver = ua.substr( uaindex + 8, 3 );
+	  }
+	  else
+	  {
+	    mobileOSver = 'unknown';
+	  }
+	}
 	
-	Path.root("#/");
-	Path.listen();
+	getOS();
+	if(mobileOS == 'iOS') {
+		$('.android').fadeOut();
+	} else if(mobileOS == 'Android') {
+		$('.ios').fadeOut();
+	}
 });
