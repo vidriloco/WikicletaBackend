eb73651d1b39ea1911f2f9d8de0be72fbabefe02
Alejandro Cruz Paz <vidriloco@gmail.com>
2013-12-31 13:57:44 -0600

    Covered sessions controller associated specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -149,6 +149,8 @@ class User < ActiveRecord::Base
     "#{id}"
   end
   
+  # Dumpables
+  
   def self.attrs_for_dump
     %w(city_id email full_name username encrypted_password reset_password_token reset_password_sent_at remember_created_at sign_in_count current_sign_in_at last_sign_in_at current_sign_in_ip last_sign_in_ip authentication_token bio personal_page started_cycling_date created_at updated_at)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,32 @@ describe User do
   it "should NOT create a new user when params are missing" do
     User.create_with(:full_name => "Pipo Perengano", :username => "pipo").errors.should_not be_empty
   end
+  
+  describe "given user Pipo registered" do
+    
+    before(:each) do
+      @pipo = FactoryGirl.create(:pipo)
+    end
+    
+    it "should be found by email" do
+      User.find_for_database_authentication(:login=>@pipo.email).should == @pipo
+    end
+    
+    it "should be found by username" do
+      User.find_for_database_authentication(:login=>@pipo.username).should == @pipo
+    end
+    
+    it "should expose itself as a dictionary" do
+      @pipo.as_json.should == {
+        "full_name" => @pipo.full_name,
+        "username" => @pipo.username,
+        "email" => @pipo.email, 
+        "bio" => @pipo.bio,
+        :identifier => @pipo.id.to_s,
+        :auth_token => @pipo.authentication_token,
+        :created_at_ms => @pipo.created_at.to_time.to_i.to_s
+      }
+    end
+    
+  end
 end
