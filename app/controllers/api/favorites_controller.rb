9b2302b0f02c799719c36fa1b459575ed242b387
Alejandro Cruz Paz <vidriloco@gmail.com>
2014-01-05 19:07:26 -0600

    Added documentation for favorites controller

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -4,24 +4,65 @@ class Api::FavoritesController < Api::BaseController
   before_filter :find_user_with_token, :only => [:mark, :unmark]
   before_filter :respond_to_bad_auth, :only => [:mark, :unmark]
   
+  api :POST, '/api/favorites/mark', "Makes sure the object with the provided details gets marked as favorited"
+  description <<-EOS
+    == About the response
+    The response for this request is always a success    
+  EOS
+  param :extras, Hash, :required => true do
+    param :auth_token, String, 'The _auth_token_ of the user received upon login on the application', :required => true
+  end
+  param :favorite, Hash, :required => true do
+    param :favorited_object_id, Integer, 'The id of the favorited object', :required => true
+    param :favorited_object_type, String, 'The kind of the favorited object', :required => true
+    param :user_id, Integer, "The id of the user who contributed the object"
+  end
   def mark
     @favorite = Favorite.mark(params[:favorite])
     
     render :json => { :success => true }, :status => :ok
   end
   
+  api :POST, '/api/favorites/unmark', "Makes sure the object with the provided details gets unmarked as favorited"
+  description <<-EOS
+    == About the response
+    The response for this request is always a success    
+  EOS
+  param :extras, Hash, :required => true do
+    param :auth_token, String, 'The _auth_token_ of the user received upon login on the application', :required => true
+  end
+  param :favorite, Hash, :required => true do
+    param :favorited_object_id, Integer, 'The id of the favorited object', :required => true
+    param :favorited_object_type, String, 'The kind of the favorited object', :required => true
+    param :user_id, Integer, "The id of the user who contributed the object"
+  end
   def unmark
     @favorite = Favorite.unmark(params[:favorite])
     
     render :json => { :success => true }, :status => :ok
   end
   
+  api :GET, '/favorites/marked/:favorited_object_id/:favorited_object_type/:user_id', "Replies with true or false to the question of whether a POI is a favorite or not of someone"
+  description <<-EOS
+    == About the response
+    The response for this request is a yes or no wrapped on a json formatted reponse similar to:
+    {"success":true,"is_favorite":true}
+    
+    A :favorited_object_id and :user_id have both a numerical value, whilst a :favorited_object_type a string one such as "Parking" or "Tip"
+  EOS
   def marked?
     @favorite = Favorite.favorite?(params)
     
     render :json => {:success => true, :is_favorite => @favorite }, :status => :ok
   end
   
+  api :GET, '/favorites/list/:user_id', "Fetches the list of favorite items the user with the given user_id has"
+  description <<-EOS
+    == About the response
+    The response for this request is an array of objects with the form:
+        {"success":true,"favorites":{"Tip":[{"title":2,"description":"En el mercado de mixcalco los pasillos son amplios puedes entrar con la bici","lat":19.434502,"lon":-99.1254,"str_updated_at":"2013-10-29 16:17:11"}]}}
+    The response within "favorites" contains, thus, a dictionary with the list of favorites grouped on categories: Tip, Parking, Workshop and so on. Inside each one, are the favorited elements on the category
+  EOS
   def list
     @favorites = Favorite.all_from_user(params[:user_id])
     
--- app/controllers/api/parkings_controller.rb
+++ app/controllers/api/parkings_controller.rb
@@ -4,6 +4,29 @@ class Api::ParkingsController < Api::BaseController
   before_filter :find_user_with_token, :only => [:create, :destroy, :update]
   before_filter :respond_to_bad_auth, :only => [:create, :destroy, :update]
   
+  api :POST, '/parkings', "Creates a new parking"
+  param :extras, Hash, :required => true do
+    param :auth_token, String, 'The _auth_token_ of the user received upon login on the application', :required => true
+  end
+  param :parking, Hash, :required => true do
+    param :details, String, 'The details for this parking', :required => true
+    param :kind, [1,2,3], 'The kind of parking, which can take on the following values: 1 for _government_provided_, 2 for _urban_mobiliary_ or 3 for _venue_provided_', :required => true
+    param :created_at, String, "Created at timestamp with format: yyyy-MM-dd'T'HH:mm:ss"
+    param :updated_at, String, "Updated at timestamp with format: yyyy-MM-dd'T'HH:mm:ss"
+    param :has_roof, TrueClass, "Whether this parking place has a roof or not"
+  end
+  param :coordinates, Hash, :required => true do 
+    param :lat, Float, 'Latitude component', :required => true
+    param :lon, Float, 'Longitude component', :required => true
+  end
+  description <<-EOS
+    == About the response
+    The response for this API method is either a 
+        success => true 
+    response with status *200* or a
+        errors => { some => errors } 
+    hash with status *422*
+  EOS
   def create
     @parking = Parking.new_with(params[:parking], params[:coordinates], @user)
 
